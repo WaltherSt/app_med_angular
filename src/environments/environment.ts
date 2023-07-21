312467b894ef1ff1960a39bc6a496397d444e392
Walter_Gomez <129338273+WalterFernandoGomezSantamaria@users.noreply.github.com>
2023-07-21 15:28:35 -0500

    update package.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
 export const environment = {
+  environment: 'production',
+  production: true,
+  urlBackend: 'https://backendmedjava-production.up.railway.app/api',
+};
+
+/* export const environment = {
   environment: 'local',
   production: false,
   urlBackend: 'http://localhost:3900/api',
-};
+}; */
